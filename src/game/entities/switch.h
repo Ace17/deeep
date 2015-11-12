ea85d89192f495931dada7d1b680a518df6dc860
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2015-11-12 19:04:28 +0100

    Copyright notices

--- src/game/entities/switch.h
+++ src/game/entities/switch.h
@@ -1,3 +1,16 @@
+/**
+ * @brief Switch and door.
+ * @author Sebastien Alaiwan
+ */
+
+/*
+ * Copyright (C) 2015 - Sebastien Alaiwan
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ */
+
 #pragma once
 
 #include <algorithm>
--- src/game/level1.cpp
+++ src/game/level1.cpp
@@ -1,3 +1,16 @@
+/**
+ * @brief Level 1
+ * @author Sebastien Alaiwan
+ */
+
+/*
+ * Copyright (C) 2015 - Sebastien Alaiwan
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ */
+
 #include <cassert>
 #include "engine/geom.h"
 #include "engine/raii.h"
