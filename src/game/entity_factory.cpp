a6cdb3760d2263f2c4b208c6d23dfaf8ad3fcda8
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2017-02-22 13:11:49 +0100

    Separate entity registration from instanciation

--- src/game/entity_factory.cpp
+++ src/game/entity_factory.cpp
@@ -1,4 +1,23 @@
 #include "entity_factory.h"
+#include <map>
+#include <functional>
+
+using namespace std;
+
+typedef function<unique_ptr<Entity>()> CreationFunc;
+static map<string, CreationFunc> getRegistry();
+static const map<string, CreationFunc> registry = getRegistry();
+
+unique_ptr<Entity> createEntity(string name)
+{
+  auto i_func = registry.find(name);
+
+  if(i_func == registry.end())
+    throw runtime_error("unknown entity type: '" + name + "'");
+
+  return (*i_func).second();
+}
+
 #include "game/entities/switch.h"
 #include "game/entities/wheel.h"
 #include "game/entities/spider.h"
@@ -6,45 +25,59 @@
 #include "game/entities/detector.h"
 #include "game/entities/bonus.h"
 #include "game/entities/player.h"
-#include "game/room.h"
 
-using namespace std;
-
-unique_ptr<Entity> createEntity(string name)
+static map<string, CreationFunc> getRegistry()
 {
-  if(name == ENTITY_UPGRADE_CLIMB)
-  {
-    return makeBonus(4, UPGRADE_CLIMB);
-  }
-  else if(name == ENTITY_UPGRADE_SHOOT)
-  {
-    return makeBonus(3, UPGRADE_SHOOT);
-  }
-  else if(name == ENTITY_UPGRADE_DASH)
-  {
-    return makeBonus(5, UPGRADE_DASH);
-  }
-  else if(name == ENTITY_BONUS_LIFE)
-  {
-    return makeBonus(0, 0);
-  }
-  else if(name == ENTITY_ENEMY_WHEEL)
-  {
-    return make_unique<Wheel>();
-  }
-  else if(name == ENTITY_ENEMY_SPIDER)
-  {
-    return make_unique<Spider>();
-  }
-  else if(name == ENTITY_FRAGILE_DOOR)
-  {
-    return make_unique<BreakableDoor>();
-  }
-  else if(name == ENTITY_TELEPORTER)
-  {
-    return make_unique<Teleporter>();
-  }
-  else
-    throw runtime_error("unknown entity type: '" + name + "'");
+  map<string, CreationFunc> r;
+
+  r[ENTITY_UPGRADE_CLIMB] =
+    [] ()
+    {
+      return makeBonus(4, UPGRADE_CLIMB);
+    };
+
+  r[ENTITY_UPGRADE_SHOOT] =
+    [] ()
+    {
+      return makeBonus(3, UPGRADE_SHOOT);
+    };
+
+  r[ENTITY_UPGRADE_DASH] =
+    [] ()
+    {
+      return makeBonus(5, UPGRADE_DASH);
+    };
+
+  r[ENTITY_BONUS_LIFE] =
+    [] ()
+    {
+      return makeBonus(0, 0);
+    };
+
+  r[ENTITY_ENEMY_WHEEL] =
+    [] ()
+    {
+      return make_unique<Wheel>();
+    };
+
+  r[ENTITY_ENEMY_SPIDER] =
+    [] ()
+    {
+      return make_unique<Spider>();
+    };
+
+  r[ENTITY_FRAGILE_DOOR] =
+    [] ()
+    {
+      return make_unique<BreakableDoor>();
+    };
+
+  r[ENTITY_TELEPORTER] =
+    [] ()
+    {
+      return make_unique<Teleporter>();
+    };
+
+  return r;
 }
 
