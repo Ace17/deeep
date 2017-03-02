01bc9c9c394072283a46528bea8b065813cef178
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2017-03-02 19:47:11 +0100

    Increase screen size

--- src/engine/app.cpp
+++ src/engine/app.cpp
@@ -54,7 +54,7 @@ public:
     SDL_Init(0);
     memset(keys, 0, sizeof keys);
 
-    Display_init(512, 512);
+    Display_init(768, 768);
     m_audio.reset(createAudio());
 
     for(auto sound : m_scene->getSounds())
