dc72c495354f7a506c8d3d7bd02c75b34ea57913
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2017-03-07 07:18:45 +0100

    Share tileset

---
+++ res/tiles-editor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles-editor" tilewidth="16" tileheight="16" tilecount="128" columns="8">
+ <image source="tiles-editor.png" width="128" height="256"/>
+</tileset>
