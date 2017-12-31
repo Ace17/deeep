0c188827482361825f79c178c92b417cf4d97fd4
Sebastien Alaiwan <Unknown>
2017-12-31 20:12:21 +0100

    Add slight vertical shading

--- engine/src/fragment.glsl
+++ engine/src/fragment.glsl
@@ -1,12 +1,14 @@
 #version 100
 precision mediump float;
 varying vec2 v_texCoord;
+varying vec4 v_position;
 uniform vec4 v_color;
 uniform sampler2D s_baseMap;
 
 void main()
 {
   gl_FragColor = texture2D(s_baseMap, v_texCoord) + v_color;
+  gl_FragColor += vec4(v_position.y, v_position.y, v_position.y, v_position.y) * 0.2;
 }
 
 // vim: syntax=glsl
--- engine/src/vertex.glsl
+++ engine/src/vertex.glsl
@@ -3,11 +3,13 @@
 attribute vec4 a_position;
 attribute vec2 a_texCoord;
 varying vec2 v_texCoord;
+varying vec4 v_position;
 uniform mat4 MVP;
 
 void main()
 {
   gl_Position = MVP * a_position;
   v_texCoord = a_texCoord;
+  v_position = gl_Position;
 }
 // vim: syntax=glsl
