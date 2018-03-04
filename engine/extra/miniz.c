f27d5f771202d459e75fc425cf74b87312fc4363
Sebastien Alaiwan <Unknown>
2018-03-04 13:09:03 +0100

    Cosmetics

--- engine/extra/miniz.c
+++ engine/extra/miniz.c
@@ -405,12 +405,14 @@ int mz_uncompress(unsigned char *pDest, mz_ulong *pDest_len, const unsigned char
 
 const char *mz_error(int err)
 {
-  static struct { int m_err; const char *m_pDesc; } s_error_descs[] =
+  static const struct { int m_err; const char *m_pDesc; } s_error_descs[] =
   {
     { MZ_OK, "" }, { MZ_STREAM_END, "stream end" }, { MZ_NEED_DICT, "need dictionary" }, { MZ_ERRNO, "file error" }, { MZ_STREAM_ERROR, "stream error" },
     { MZ_DATA_ERROR, "data error" }, { MZ_MEM_ERROR, "out of memory" }, { MZ_BUF_ERROR, "buf error" }, { MZ_VERSION_ERROR, "version error" }, { MZ_PARAM_ERROR, "parameter error" }
   };
-  mz_uint i; for (i = 0; i < sizeof(s_error_descs) / sizeof(s_error_descs[0]); ++i) if (s_error_descs[i].m_err == err) return s_error_descs[i].m_pDesc;
+  for (unsigned int i = 0; i < sizeof(s_error_descs) / sizeof(s_error_descs[0]); ++i)
+    if (s_error_descs[i].m_err == err)
+      return s_error_descs[i].m_pDesc;
   return NULL;
 }
 
