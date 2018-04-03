1d984750e1bae7ca597bb16d99dae664c3f38c5e
Sebastien Alaiwan <Unknown>
2018-04-03 13:11:52 +0200

    Upgrade shaders (use Webgl 2.0)

--- engine/src/fragment.glsl
+++ engine/src/fragment.glsl
@@ -1,14 +1,22 @@
-#version 100
+#version 300 es
+
 precision mediump float;
-varying vec2 v_texCoord;
-varying vec4 v_position;
+
+// Interpolated values from the vertex shader
+in vec2 v_texCoord;
+in vec4 v_position;
+
+// Ouput data
+out vec4 color;
+
+// Values that stay constant for the whole mesh
 uniform vec4 v_color;
 uniform sampler2D s_baseMap;
 
 void main()
 {
-  gl_FragColor = texture2D(s_baseMap, v_texCoord) + v_color;
-  gl_FragColor += vec4(v_position.y, v_position.y, v_position.y, 0) * 0.2;
+  color = texture2D(s_baseMap, v_texCoord) + v_color;
+  color += vec4(v_position.y, v_position.y, v_position.y, 0) * 0.2;
 }
 
 // vim: syntax=glsl
--- engine/src/vertex.glsl
+++ engine/src/vertex.glsl
@@ -1,10 +1,11 @@
-#version 100
+#version 300 es
 
-attribute vec2 a_position;
-attribute vec2 a_texCoord;
+in vec2 a_position;
+in vec2 a_texCoord;
 
-varying vec2 v_texCoord;
-varying vec4 v_position;
+out vec2 v_texCoord;
+out vec4 v_position;
+
 uniform mat4 MVP;
 
 void main()
