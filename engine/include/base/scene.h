9a773df386c6bdf39c2db8f669a27935f721135b
Sebastien Alaiwan <Unknown>
2018-08-08 08:47:32 +0200

    Engine: homogenize copyright notices

--- engine/include/base/scene.h
+++ engine/include/base/scene.h
@@ -1,10 +1,8 @@
-/*
- * Copyright (C) 2017 - Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- */
+// Copyright (C) 2018 - Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 
 // Game, as seen by the outside world
 
