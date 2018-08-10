75c142b9257507707b5700ed3845e8a939cd1469
Sebastien Alaiwan <Unknown>
2018-08-10 08:38:50 +0200

    Shader: add comments

--- engine/src/vertex.glsl
+++ engine/src/vertex.glsl
@@ -1,11 +1,14 @@
 #version 300 es
 
+// Input vertex data, different for all executions of this shader
 in vec2 a_position;
 in vec2 a_texCoord;
 
+// Output data; will be interpolated for each fragment
 out vec2 v_texCoord;
 out vec4 v_position;
 
+// Values that stay constant for the whole mesh.
 uniform mat4 MVP;
 
 void main()
