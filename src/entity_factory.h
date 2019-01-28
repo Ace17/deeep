2be9c8dbf55d3a0b8a21bc528afc864fcc54167b
Sebastien Alaiwan <Unknown>
2019-01-28 13:04:05 +0100

    Cleanup includes

--- src/entity_factory.h
+++ src/entity_factory.h
@@ -1,22 +1,19 @@
-/*
- * Copyright (C) 2017 - Sebastien Alaiwan
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- */
+// Copyright (C) 2019 - Sebastien Alaiwan
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 
 #pragma once
-
-struct Entity;
 
 #include <functional>
 #include <string>
 #include <vector>
 #include <memory>
-#include <map>
 
 using namespace std;
+
+struct Entity;
 
 // e.g:
 // createEntity("spider");
