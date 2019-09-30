8dd7f7ecb960d593debc334f38a944ce312ad844
Sebastien Alaiwan <Unknown>
2019-09-30 08:42:17 +0200

    Simplify singleton creation (Thanks to Romain for showing me this trick)

--- src/entity_factory.cpp
+++ src/entity_factory.cpp
@@ -15,32 +15,24 @@ using namespace std;
 
 namespace
 {
-// don't put a unique_ptr here, as this would make us
-// depend on the module initialization order
-map<string, CreationFunc>* g_registry;
-
-struct RegistryDeleter
+map<string, CreationFunc>& g_registry()
 {
-  ~RegistryDeleter() { delete g_registry; }
-};
-
-RegistryDeleter g_deleteRegistryAtProgramExit;
+  static map<string, CreationFunc> registry;
+  return registry;
+}
 }
 
 int registerEntity(string type, CreationFunc func)
 {
-  if(!g_registry)
-    g_registry = new map<string, CreationFunc>;
-
-  (*g_registry)[type] = func;
+  g_registry()[type] = func;
   return 0; // ignored
 }
 
 unique_ptr<Entity> createEntity(string name, IEntityConfig* args)
 {
-  auto i_func = g_registry->find(name);
+  auto i_func = g_registry().find(name);
 
-  if(i_func == g_registry->end())
+  if(i_func == g_registry().end())
     throw runtime_error("unknown entity type: '" + name + "'");
 
   return (*i_func).second(args);
