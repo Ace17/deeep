e7dd194963f2fd610c2d80ca91403b13d8295c28
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2020-04-26 16:08:14 +0200

    Make debug-mode a toggle

--- engine/src/app.cpp
+++ engine/src/app.cpp
@@ -140,6 +140,7 @@ private:
     {
       m_scene.release();
       m_scene.reset(next);
+      printf("Entering: %s\n", typeid(*next).name());
     }
   }
 
@@ -174,7 +175,8 @@ private:
     m_control.dash = keys[SDL_SCANCODE_C];
 
     m_control.restart = keys[SDL_SCANCODE_R];
-    m_control.debug = keys[SDL_SCANCODE_SCROLLLOCK];
+
+    m_control.debug = m_debugMode;
   }
 
   void draw()
@@ -191,8 +193,13 @@ private:
       m_display->drawText(Vector2f(0, 0), "PAUSE");
     else if(m_slowMotion)
       m_display->drawText(Vector2f(0, 0), "SLOW-MOTION MODE");
-    else if(m_control.debug)
-      m_display->drawText(Vector2f(0, 0), "DEBUG MODE");
+
+    if(m_debugMode)
+    {
+      char debugText[256];
+      sprintf(debugText, "FPS: %d", m_fps.slope());
+      m_display->drawText(Vector2f(0, -4), debugText);
+    }
 
     if(m_textboxDelay > 0)
     {
@@ -231,7 +238,7 @@ private:
         return;
       }
 
-      m_captureFile = fopen("capture.rgb", "wb");
+      m_captureFile = fopen("capture.rgba", "wb");
 
       if(!m_captureFile)
       {
@@ -265,6 +272,10 @@ private:
 
   void onKeyDown(SDL_Event* evt)
   {
+    keys[evt->key.keysym.scancode] = 1;
+
+    if(evt->key.repeat > 0)
+      return;
     switch(evt->key.keysym.sym)
     {
     case SDLK_ESCAPE:
@@ -287,16 +298,13 @@ private:
 
     case SDLK_PRINTSCREEN:
       {
-        if(evt->key.repeat == 0)
+        if(evt->key.keysym.mod & KMOD_CTRL)
         {
-          if(evt->key.keysym.mod & KMOD_CTRL)
-          {
-            toggleVideoCapture();
-          }
-          else
-          {
-            m_mustScreenshot = true;
-          }
+          toggleVideoCapture();
+        }
+        else
+        {
+          m_mustScreenshot = true;
         }
 
         break;
@@ -305,12 +313,14 @@ private:
     case SDLK_RETURN:
       {
         if(evt->key.keysym.mod & KMOD_LALT)
-        {
-          if(evt->key.repeat == 0)
-          {
-            toggleFullScreen();
-          }
-        }
+          toggleFullScreen();
+
+        break;
+      }
+
+    case SDLK_SCROLLLOCK:
+      {
+        m_debugMode = !m_debugMode;
 
         break;
       }
@@ -322,8 +332,6 @@ private:
         break;
       }
     }
-
-    keys[evt->key.keysym.scancode] = 1;
   }
 
   void onKeyUp(SDL_Event* evt)
@@ -391,6 +399,8 @@ private:
   int m_fixedDisplayFramePeriod = 0;
   FILE* m_captureFile = nullptr;
   bool m_mustScreenshot = false;
+
+  bool m_debugMode = false;
 
   int m_lastTime;
   int m_lastDisplayFrameTime;
