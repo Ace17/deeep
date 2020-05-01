cd3c9b64bfb8b73e3d309658143a3c2b29c8b5d0
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2020-05-01 11:36:28 +0200

    Unbind texture after loading

--- engine/src/render/display_ogl.cpp
+++ engine/src/render/display_ogl.cpp
@@ -187,14 +187,14 @@ int loadTexture(const char* path, Rect2f frect)
   GLuint texture;
 
   glGenTextures(1, &texture);
+
   glBindTexture(GL_TEXTURE_2D, texture);
-
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rect.size.width, rect.size.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img.data());
-
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glBindTexture(GL_TEXTURE_2D, 0);
 
   return texture;
 }
