85e46f44afec642dd42a85f9ebc3f2b028eb2989
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2024-10-08 13:28:03 +0200

    Fixup copyright

--- src/render/png.cpp
+++ src/render/png.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2021 - Sebastien Alaiwan
-// Copyright (C) 2021 - Sebastien Alaiwan
+// Copyright (c) 2005-2010 Lode Vandevenne
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
